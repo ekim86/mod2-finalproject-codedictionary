284d29596e107aef723fa4c65c71d50109c851ec
github username <github email address>
2020-01-21 12:43:06 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+UserTerm.destroy_all
+Term.destroy_all
+Language.destroy_all
+
+u1 = User.create!(name: "Brinda", username: "b", password_digest: "1234")
+u2 = User.create!(name: "Eunice", username: "e", password_digest: "1234")
+
+l1 = Language.create!(name: "Ruby")
+l2 = Language.create!(name: "JavaScript")
+
+t1 = Term.create!(name: "select", content: "Returns an array containing 
+  all elements of enum for which the given block returns a true value. 
+  The find_all and select methods are aliases. There is no performance 
+  benefit to either. If no block is given, an Enumerator is returned instead.", language: l1)
+t2 = Term.create!(name: "map", content: "Returns a new array with the 
+  results of running block once for every element in enum. If no block 
+  is given, an enumerator is returned instead.", language: l1)
+
+UserTerm.create!(user: u1, term: t1)
+UserTerm.create!(user: u1, term: t2)
+UserTerm.create!(user: u2, term: t2)
